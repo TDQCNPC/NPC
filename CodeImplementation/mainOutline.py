88105f41759f2b58f350e1e186c84352262f97ca
spheredmantis57 <81576411+spheredmantis57@users.noreply.github.com>
2021-04-01 11:04:43 -0400

    Update mainOutline.py

--- CodeImplementation/mainOutline.py
+++ CodeImplementation/mainOutline.py
@@ -12,9 +12,9 @@ def main():
                 Paul.Action.flee()
         if seen:
             Paul.state = "wave"
-        elif player.Position[1] == (Paul.Position[1]+1) and (player.Position[0] == paul.Position[0])
+        if player.Position[1] == (Paul.Position[1]+1) and (player.Position[0] == paul.Position[0])
             Paul.state = input()
-        elif Paul.state == "Buying":
+        if Paul.state == "Buying":
             Paul.action.buy(read)
         elif Paul.state == "Selling":
             Paul.action.sell(read)
